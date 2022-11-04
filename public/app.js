497e98a66cc26cc4ed31078183e0013e154a600b
aymeric s <sandoz.aymeric@gmail.com>
2022-11-04 12:56:44 +0100

    config

--- public/app.js
+++ public/app.js
@@ -3,7 +3,7 @@ const listePoke = document.querySelector(".liste-poke");
 //////////Get your own cards
 console.log("banane");
 const fetchOwnerCardList = () => {
-  fetch("http://localhost:5000/api/card/", {
+  fetch("api/card/", {
     method: "GET",
   })
     .then((reponse) => reponse.json())
